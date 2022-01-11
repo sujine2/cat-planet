eefc2c49711ef9983d193308c8ecc679597a4688
hwang sujin <sujin@hwangs-MacBook-Pro.local>
2022-01-11 17:59:54 +0900

    second

--- contracts/mintNFT.sol
+++ contracts/mintNFT.sol
@@ -13,94 +13,108 @@ contract mintNFT is ERC721 {
         uint32 metDay;
         uint32 leftDay;
         address owner; //컬러정보 불러오기 위함 
+        color catColor;
     }
 
     struct color {
-        bytes2 R;
-        bytes2 G; 
-        bytes2 B;
-        bytes1 fullFlag;
+        uint8 R;
+        uint8 G; 
+        uint8 B;
+        bool set;
     }
 
     //_owners : tokenID -> owner address ,   <cat 토큰 -> owner>
     //_balance : owner -> token count,       <onwer -> cat's number>
     //token ID for mapping
     uint256 public tokenID;
+    //color 
+    color currentColor;
+    bytes32 colorHex;
+    //색 설정시 사용
+    bool fullFlag = false;
+    bool turnFlag = false;
 
     //mapping from token ID to cat's data    <cat 토큰 -> cataData>
     mapping (uint256 => catData) private _catData;
     //mapping from owner to owner's color    <owner -> color>
     mapping (address => color) private _myColor;
     //mapping from owner to owner's color    <color -> owner>
-    mapping (color => address) private _whoColor;
-    //mapping from owner to owner's color    <onwer -> cat 토큰>
-    mapping (address => tokenID) private _myCats;
+    mapping (bytes32 => address) private _whoColor;
 
-    //color 
-    color currentColor;
 
     //initialize
-    constructor() public ERC721("elyCat", "EC") {
+    constructor() ERC721("elyCat", "EC") {
         tokenID = 0;
-        currentColor.R = -1;
-        currentColor.G = -1;  
-        currentColor.B = -1;
-        currentColor.fullFlag = 0;
+        currentColor.R = 255;
+        currentColor.G = 255;  
+        currentColor.B = 255;
     }
+            
 
-    function setData(
-        string catName,
-        string yourName,
-        string comment,
-        string favorite,
-        uint32 metDay,
-        uint32 leftDay,
-        address owner
-    ) internal {
-        require(currentColor.fullFlag != 1, "The planet's stars were full");
-        _catData[tokenID] = catData(catName, yourName, comment, favorite, metDay, leftDay, msg.sender);
-    }
+    function _setColorInfo() private {
 
+        if(turnFlag == true){
+            currentColor.R--;
+            currentColor.G = 255;
+            currentColor.B = 255;
+        }
 
-    //mint 내용
-    function _beforeTokenTransfer(address _owner) internal {
-        if (currentColor.R < 256) {
-            currentColor.R++;
-        } else if(currentColor.G < 256)  {
-            currentColor.G++;
-        } else if(currentColor.B < 256) {
-            currentColor.B++;
-            if(currentColor.B == 256){
-                currentColor.fullFlag = 1;
+        if(currentColor.G == currentColor.B){
+            currentColor.G--;
+        }
+        else {
+            currentColor.B--;
+            if(currentColor.B == 0){
+                turnFlag = true;
+            }
+            if(currentColor.B == 0 && currentColor.R == 0){
+                fullFlag = true;
             }
         }
-            
-        _myCats[_owner] = tokenID;
-        if(!_myColor[_owner]) {   
-            _myColor[_owner] = currentColor; //msg.sender
-            _whoColor[currentColor] = _owner;
+
+        colorHex = keccak256(abi.encode(currentColor.R , currentColor.G , currentColor.B));  
+        currentColor.set = true;
+        _myColor[msg.sender] = currentColor; 
+        _whoColor[colorHex] = msg.sender;
+    }
+
+
+    function mint(
+        string memory catName,
+        string memory yourName,
+        string memory comment,
+        string memory favorite,
+        uint32 metDay,
+        uint32 leftDay
+    ) public {
+        require(fullFlag == false, "The planet's stars were full");
+        require(tokenID >= 0, "This is not valid token");
+        if(_myColor[msg.sender].set == false){
+            _setColorInfo();
         }
+        _catData[tokenID] = catData(catName, yourName, comment, favorite, metDay, leftDay, msg.sender, currentColor);
+        _mint(msg.sender,tokenID);
+        tokenID++;
     }
 
-    //min NFT
-    function mint(address _owner) external {
-        require(currentColor.fullFlag != 1, "The planet's stars were full");
-        require(tokenID > -1, "This is not valid token");
-        require(msg.sender == _owner);
-        _mint(_owner,tokenID);  //_balances, _owners update
-
-    }
     
 
     // return functions
 
-    function catDataOf (uint _tokenID) public view {
-        require(tokenID > -1, "This is not valid token");
+    function test() public view returns(color memory){
+        return currentColor;
+    }
+
+    function catDataOf (uint256 _tokenID) public view returns (catData memory) {
+        require(tokenID >= 0, "This is not valid token");
         return _catData[_tokenID];
     }
-    function myColorOf (uint _owner) public view {
+    function myColorOf (address _owner) public view returns (color memory) {
         require(_owner != address(0), "This is invalid address");
         return _myColor[_owner];
     }
 
+    function whoColorOf (bytes32 _color) public view returns (address) {
+        return _whoColor[_color];
+    }
 }
